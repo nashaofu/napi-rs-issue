e771459c72ee40dbb10acab81f3c8ced33027290
nashaofu <diaocheng@outlook.com>
2022-10-21 09:48:22 +0800

    feat: 修改示例代码

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![deny(clippy::all)]
 
-use napi::bindgen_prelude::Buffer;
+use napi::bindgen_prelude::*;
 
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
 pub fn get_buffer() -> Buffer {
-  Buffer::from(vec![0,0,0])
+  let v = vec![0, 0, 1];
+  Buffer::from(v)
 }
